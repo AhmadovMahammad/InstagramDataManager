b98c554c895bd5d1da278b169f3e1eb626377e97
MahammadA <mahammada@risk.az>
2025-01-11 00:55:14 +0400

    Reordered CommandType enum values for better logical grouping

--- DataManager/Constant/Enums/CommandType.cs
+++ DataManager/Constant/Enums/CommandType.cs
@@ -3,8 +3,23 @@
 namespace DataManager.Constant.Enums;
 public enum CommandType
 {
+    [Description("Provides instructions on how to get JSON or HTML files from Instagram.")]
+    Get_Info = 1,
+
+    [Description("Manage your followers.")]
+    Manage_Followers,
+
+    [Description("Manage recently unfollowed users.")]
+    Manage_Recently_Unfollowed,
+
+    [Description("Unlikes all posts you have liked.")]
+    Unlike_All_Posts,
+
     [Description("Manage follow requests.")]
-    Manage_Recent_Follow_Requests = 1,
+    Manage_Recent_Follow_Requests,
+
+    [Description("Manage follow requests that are still pending.")]
+    Manage_Pending_Follow_Requests,
 
     [Description("Manage all received follow requests.")]
     Manage_Received_Requests,
@@ -15,21 +30,6 @@ public enum CommandType
     [Description("Manage your close friends list.")]
     Manage_Close_Friends,
 
-    [Description("Manage your followers.")]
-    Manage_Followers,
-
-    [Description("Manage recently unfollowed users.")]
-    Manage_Recently_Unfollowed,
-
-    [Description("Manage follow requests that are still pending.")]
-    Manage_Pending_Follow_Requests,
-
-    [Description("Unlikes all posts you have liked.")]
-    Unlike_All_Posts,
-
-    [Description("Provides instructions on how to get JSON or HTML files from Instagram.")]
-    Get_Info,
-
     [Description("Makes extra room for the user by clearing the console.")]
-    Clear_Console
+    Clear_Console,
 }
